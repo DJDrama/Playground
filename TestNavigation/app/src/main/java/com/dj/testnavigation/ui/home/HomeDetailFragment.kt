9e96e0a57b400affd2476a505630a70300f7ea6c
DJ <putzzang@gmail.com>
2020-04-03 15:02:04 +0900

    [Navigation] Navigate Into using action

---
+++ TestNavigation/app/src/main/java/com/dj/testnavigation/ui/home/HomeDetailFragment.kt
@@ -0,0 +1,8 @@
+package com.dj.testnavigation.ui.home
+
+import androidx.fragment.app.Fragment
+import com.dj.testnavigation.R
+
+class HomeDetailFragment: Fragment(R.layout.fragment_home_detail){
+
+}
--- TestNavigation/app/src/main/java/com/dj/testnavigation/ui/home/HomeFragment.kt
+++ TestNavigation/app/src/main/java/com/dj/testnavigation/ui/home/HomeFragment.kt
@@ -1,8 +1,19 @@
 package com.dj.testnavigation.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.dj.testnavigation.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment: Fragment(R.layout.fragment_home){
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigate_to_home_detail_button.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_homeDetailFragment)
+        }
+    }
 }
